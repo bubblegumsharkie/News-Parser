8822d545fda19bac816e4d6445bd367ef5cd5cbc
Alexey Kozlov <kozlov.alex.dev@gmail.com>
2022-06-22 15:47:58 +0300

    init commit

---
+++ src/main/java/com/studyhardpro/News/Parcer/NewsParcerApplication.java
@@ -0,0 +1,13 @@
+package com.studyhardpro.News.Parcer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsParcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsParcerApplication.class, args);
+	}
+
+}
